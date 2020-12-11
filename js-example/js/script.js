86f9a3bc9d4adaf0b347d2f44dbd1deca1d295a6
Alessandro Liggieri <smmb@live.it>
2020-12-11 12:54:12 +0100

    Live coding js

--- js-example/js/script.js
+++ js-example/js/script.js
@@ -2,7 +2,12 @@ var risposta = prompt('Come ti chiami?');
 console.log('Il tizio si chiama: ' + risposta);
 // document.writeln(risposta);
 var outputContainer = document.getElementById('output');
-outputContainer.innerText = risposta;
+outputContainer.innerText = 'ciao ' + risposta;
 // funzione per "appendere", aggiungere, alla variabile outputContainer il suo argomento.
 outputContainer.append('\nquesto è un append');  // diverso da innerHTML, perché lo "aggiunge".
 //^variabile    ^funzione        ^argomento/parametro
+var age = prompt(' Ciao ' + risposta + '. Quanti anni hai?');
+console.log('Il tizio ha: ' + age + ' anni');
+var annoCorrente = new Date().getFullYear();
+var annoDiNascita = annoCorrente - age;
+outputContainer.append('\nla tua data di nascita è ' + annoDiNascita);
