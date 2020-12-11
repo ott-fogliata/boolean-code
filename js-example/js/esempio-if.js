168dd577f874a5aad2a97ebea2749cd24d2bc0fd
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2020-12-11 12:23:13 +0100

    Update esempio-if.js

--- js-example/js/esempio-if.js
+++ js-example/js/esempio-if.js
@@ -2,7 +2,7 @@
 // se la risposta è {strettamente} uguale a Pippo
 // allora esegui l'istruzione (scrivi Ciao Amico)
 if(risposta === 'Pippo') {  // comparazione
-  outputContainer.append('\nCiao AMICO!') // <-- istruzione
+  outputContainer.append('\nCiao ' + riposta) // <-- istruzione
 } else { // ALTRIMENTI.
   outputContainer.append('\nUè, e chi sei tu.')
 }
