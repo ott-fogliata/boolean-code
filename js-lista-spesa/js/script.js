75f3d6fd24757d7a14b999436a01b67ece0c7dc4
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2020-12-16 13:00:35 +0100

    js lista spesa

--- js-lista-spesa/js/script.js
+++ js-lista-spesa/js/script.js
@@ -1,6 +1,13 @@
 var listaOutput = document.getElementById('lista-spesa');
 var listaSpesa = ['Latte', 'Cacao', 'insalata', 'carote', 'tofu', 'caffè', 'vino primitivo']
+
+var contenutoPrecedente;
+
 for (var i = 0; i < listaSpesa.length; i++) {
-  var contenutoPrecedente = listaOutput.innerHTML;
+
+  contenutoPrecedente = listaOutput.innerHTML;
   listaOutput.innerHTML = contenutoPrecedente + '<li>' + listaSpesa[i] + '</li>';
+
+  //listaOutput.innerHTML += '<li>' + listaSpesa[i] + '</li>'
+
 }
