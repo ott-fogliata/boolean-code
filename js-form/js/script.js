81878e2d13e65a6b4a091d7b782c21040db7423b
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2020-12-21 11:00:38 +0100

    js-form

---
+++ js-form/js/script.js
@@ -0,0 +1,22 @@
+// var input_inserito = document.getElementsByTagName('input')[0].value;
+//
+// listaCheck = document.getElementsByClassName('checkboxes-container')[0].getElementsByTagName('input');
+
+var primoInput = document.getElementsByClassName('primo-testo')[0];
+var secondoInput = document.getElementsByClassName('altro-testo')[0];
+var checkboxes = document.getElementsByClassName('checkboxes-container')[0].getElementsByTagName('input');
+
+var testoMessaggio = 'Benvenuto! ';
+
+document.getElementById("button-submit").addEventListener("click", function() {
+  testoMessaggio += ' Il valore del primo input è ' + primoInput.value + '.';
+  testoMessaggio += ' Il valore del secondo input è ' + secondoInput.value;
+  var counterCheckedElements = 0;
+  for(var i = 0; i < checkboxes.length; i++) {
+    if(checkboxes[i].checked === true) {
+      counterCheckedElements += 1;
+    }
+  }
+  testoMessaggio += ' Hai checkato ' + counterCheckedElements + ' elementi';
+  alert(testoMessaggio);
+});
