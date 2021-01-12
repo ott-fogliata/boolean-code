079933fa000c9d0bd594f0c1e351f41b2624cb81
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-01-12 12:59:31 +0100

    jquery events

--- jquery/js/script.js
+++ jquery/js/script.js
@@ -9,3 +9,33 @@ $('#elemento, #elemento-2')
 
 // il selettore jQuery è praticamente il selettore CSS
 red('p');
+
+
+// nostalgia canaglia (js puro <3)
+// var button = document.getElementById('auto-button-click');
+// button.addEventListener('click', function() {
+//   alert('Cliccato!')
+// });
+
+// jquery superPower!
+// jquery clicca per noi!
+// $('#auto-button-click').click();
+
+// tutto jquery
+ $('#auto-button-click').click(function() {
+   alert('Cliccato!')
+ });
+
+ // un modo diverso di vedere le cose
+function urla() {
+  alert('AAAAAAAAAAAAHHHHH!');
+}
+$('#auto-button-click2').click(urla);
+
+// un modo diverso di vedere le cose, ma con argomenti
+function urlaConArgomenti(argomento) {
+ alert('AAAAAAAAAAAAHHHHH ' + argomento + '!!!!!');
+}
+$('#auto-button-click2').click(function () {
+  urlaConArgomenti('jQuery')
+});
