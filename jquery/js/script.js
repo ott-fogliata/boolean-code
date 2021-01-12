cb32239705ac5436feb56b9242d3a9f27cdb0599
Matteo Basile <matteo.basile56@gmail.com>
2021-01-12 11:57:23 +0100

    jquery addClass

--- jquery/js/script.js
+++ jquery/js/script.js
@@ -1,1 +1,11 @@
-$('#nascondimi').fadeIn();
+// $('#elemento').addClass('red');
+function red(selettore) {
+  $(selettore).addClass('red');
+}
+
+$('#elemento, #elemento-2')
+  .removeClass('classe-1')
+  .addClass('red');
+
+// il selettore jQuery è praticamente il selettore CSS
+red('p');
