8ca9bb2365a3e200e2ce30d9146f9ee41bf773cf
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-01-15 11:40:14 +0100

    function callback

---
+++ js-callback/script.js
@@ -0,0 +1,23 @@
+function urla() {
+  alert('AAAAAAAAAAH!');
+}
+
+function start(pippoBack) {
+  console.log('ho caricato la pagina');
+
+  if(typeof pippoBack === 'function') {
+    // callback
+    pippoBack();  // è come aver scritto urla();
+
+  }
+}
+
+// viene eseguito al caricamento in pagina
+start(urla)
+
+
+// funzione anonima
+
+start(function(
+  alert('ginoooooooo!')
+))
