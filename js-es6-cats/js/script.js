7a668b819779b427ab0997621d94fa309f752690
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-01-21 13:06:34 +0100

    starting a filter

--- js-es6-cats/js/script.js
+++ js-es6-cats/js/script.js
@@ -13,8 +13,20 @@ const cats = [
     gender: 'male'
   },
   {
+    name: 'Romeo',
+    age: 4,
+    color: 'black',
+    gender: 'male'
+  },
+  {
     name: 'Emily',
     age: 12,
+    color: 'orange',
+    gender: 'female'
+  },
+  {
+    name: 'Alba',
+    age: 18,
     color: 'orange',
     gender: 'female'
   },
@@ -103,3 +115,90 @@ maleCats.forEach((element) => {
 //
 // insertCat(femaleCats, boxFemale, 'pink')
 // insertCat(maleCats, boxMale, 'blue')
+
+
+// MILESTONE 3 -----------------------------------------------------------------------
+
+/*
+
+const femaleCats = cats.filter((element) => {
+  return element.gender === 'female'
+})
+
+const maleCats = cats.filter((element) => {
+  return element.gender === 'male'
+})
+
+
+*/
+
+const containerFinalList = document.getElementById('container-final-list');
+
+const orderedCatList = [...femaleCats, ...maleCats];
+
+orderedCatList.forEach((item) => {
+
+  const {age, color, name, gender} = item;
+
+  const opacity = age / maxAge;
+
+  let ribbonColor;
+
+  if(gender === 'male') {
+     ribbonColor = 'blue';
+  } else {
+    ribbonColor = 'pink';
+  }
+
+  /*
+    let ribbonColor = 'pink';
+    if(gender === 'male') {
+       ribbonColor = 'blue';
+    }
+  */
+
+  containerFinalList.innerHTML += `
+    <div>
+      <i class="fas fa-cat" style="color:${color}"></i>
+      <i class="fas fa-ribbon" style='color:${ribbonColor}; opacity:${opacity}'></i>
+      ${name}
+    </div>
+  `
+});
+
+// --- RICERCA CON SELECT ---------------------------------------------------------------
+
+
+const boxFiltered = document.getElementById('box-filtered');
+const colorSelect = document.getElementsByClassName('color-select')[0];
+
+const colorList = [];
+cats.forEach((element) => {
+  if(!colorList.includes(element.color)) {
+    colorList.push(element.color);
+  }
+})
+
+colorList.forEach((element) => {  // element è la stringa color
+  colorSelect.innerHTML += `
+    <option value="${element}">${element}</option>
+  `
+});
+
+cats.forEach((element) => {
+
+  boxFiltered.innerHTML += `
+  <div>
+    <i class="fas fa-cat" style="color:${element.color}"></i>
+    ${element.name}
+  </div>
+  `
+
+})
+
+const selector = $('.color-select');
+
+selector.change(function() {
+  let valore = $(this).val();
+  alert(valore);
+})
