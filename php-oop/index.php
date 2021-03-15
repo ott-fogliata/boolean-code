1f39bc6c2d3188d3282530972d367a4367467315
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-03-15 12:13:05 +0100

    auto is now able to get diesel

--- php-oop/index.php
+++ php-oop/index.php
@@ -13,6 +13,11 @@ class Automobile {
 
     public $nome;
 
+    /** int $serbatoio */
+    private $serbatoio = 20;  // numero di litri 
+
+    public $serbatoioMax = 100;
+
     public function __construct($motore, $cerchi, $telaio, $cambio, $nome) {
         $this->motore = $motore;
         $this->cerchi = $cerchi;
@@ -21,8 +26,32 @@ class Automobile {
         $this->nome = $nome;
     }
 
+    // getter
+    public function getSerbatoio() 
+    {
+        return $this->serbatoio;
+    }
+
+    // setter 
+    public function fillSerbatoio($diesel) 
+    {
+        $this->serbatoio += $diesel;
+    }
+
+    public function isSerbatoioFull() {
+        return $this->serbatoio === $this->serbatoioMax;
+    }
+
 }
 
-
 $mito = new Automobile('jtd 16', '16"', 'tubolare', 'automatico', 'alfa mito');
-echo $mito->cambio;
+echo $mito->motore . '<br/>';
+echo $mito->getSerbatoio() . '<br/>';
+
+if(!$mito->isSerbatoioFull()) {
+    // il massimo del serbatoio meno quanti litri ci sono adesso nel serbatoio
+    $diesel = $mito->serbatoioMax - $mito->getSerbatoio();
+    $mito->fillSerbatoio($diesel);
+}
+
+echo $mito->getSerbatoio() . '<br/>';
